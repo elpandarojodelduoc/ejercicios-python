541c1c1c7120c7a4c2b162b87b00ddfbe4c15214
unknown <Cetecom@SJAPCA03LC0708.duoc.acad>
2025-06-11 14:15:20 -0400

    mi primer commit

---
+++ sas.py
@@ -0,0 +1,32 @@
+# def saludos(nombre = "juan"):      #parametro
+#     print(F"hola, {nombre}")
+
+# saludos("Renato")         #Argumento
+
+
+
+# def sumar(a, b):
+#     print(a + b)
+
+# sumar(3, 4)
+
+
+# def multiplicacion(x, y):
+#     return x * y
+# resultado = multiplicacion(2,4)
+# print(resultado)
+
+# def mostrar():
+#     mensaje = "hola"
+#     print(mensaje)
+
+# mostrar()
+
+# num % 2 == 0
+
+def numero(num):
+    if num % 2 == 0:
+        print(f"el numero {num} es par")
+    else:
+        print(f"el numero {num} es impar")
+numero(10)
